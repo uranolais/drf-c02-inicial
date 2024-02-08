1b01fc11128d6bcd4f2771a04345d1c7a6990d53
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-02-08 19:54:15 -0300

    adicionando validações no model de Curso

--- escola/serializers.py
+++ escola/serializers.py
@@ -20,6 +20,7 @@ class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
+    
 
 class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
